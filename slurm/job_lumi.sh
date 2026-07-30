c9dd98e89ef930bbf9b69caaed81cadd761f55fe
Matyas Constans <mconstanshu@gmail.com>
2026-07-31 01:52:45 +0800

    rank

--- slurm/job_lumi.sh
+++ slurm/job_lumi.sh
@@ -3,6 +3,7 @@
 #SBATCH --partition=standard
 #SBATCH --ntasks-per-node=8
 #SBATCH --cpus-per-task=16
+#SBATCH --threads-per-core=1
 #SBATCH --time=00:30:00
 #SBATCH --account=project_465002685
 
@@ -10,4 +11,6 @@ module load LUMI
 module load partition/C
 module load cpeGNU
 
-srun --cpu-bind=verbose,map_cpu:0,16,32,48,64,80,96,112 redsim_cpu "$1"
+srun --cpu-bind=verbose,rank_ldom ./redsim_cpu "$1"
+
+
