48dca43242d8598eba12a21ee222dfc5ca2e64c6
Matyas Constans <matyas.constans@math.sze.hu>
2026-08-17 00:30:31 +0200

    patched

--- slurm/job_solyom.sh
+++ slurm/job_solyom.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=redsim
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=16
-
 module load openmpi/4.1.2
-srun --cpu-bind=verbose,rank_ldom ./redsim_cpu "$1"
+# srun --cpu-bind=verbose,rank_ldom ./redsim_cpu "$1"
+mpirun --bind-to numa --report-bindings --map-by ppr:2:node ./redsim_cpu "$1"
 
